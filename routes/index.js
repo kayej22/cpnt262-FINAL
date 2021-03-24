e5da3dc46bd57097c6ef26051a72d86b1097256d
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-03-24 13:42:59 -0600

    added home, subscribe. team and admin endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,28 @@
 const express = require('express')
 const router = express.Router()
 const api = require('./api/v0')
+
+// Home
+router.get('/', (req, res) => {
+  res.render('pages/index', {pageTitle: 'Home'})
+})
+
+// Subscribe
+router.get('/subscribe', (req, res) => {
+  res.render('pages/subscribe', {pageTitle: 'Subscribe'})
+})
+
+// Team
+router.get('/team', (req, res) => {
+  res.render('pages/team', {pageTitle: 'Team'})
+})
+
+// Admin
+router.get('/admin', (req, res) => {
+  res.render('pages/admin', {pageTitle: 'Admin'})
+})
+
+router.use('/api/v0/', api);
+
+module.exports = router
+
