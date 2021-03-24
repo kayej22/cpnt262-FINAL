bc0c20011522bffe883845a69e393a378e0b1801
Aidan OReilly <ajoreilly93@gmail.com>
2021-03-24 13:44:35 -0600

    initial api set up

---
+++ routes/api/v0.js
@@ -0,0 +1,55 @@
+const express = require('express');
+/// Model requires go here ////
+
+const router = express.Router();
+
+
+
+//gallery JSON
+router.get('/gallery', (req, res) => {
+
+  //find() method after models have been built ///
+
+  // Image.find({}, (err, images) => {
+  //   if (err) {
+  //     res.status(404)
+    
+  //   }
+  //   res.json(images)
+  // })
+})
+
+//single image JSON
+
+router.get('/images/id', async (req, res, next) => {
+
+  //find() method after models have been built ///
+
+  // try {
+  //   const gallery = await Image.find({});
+  //   if (gallery) return res.json(gallery);
+  //   return next(new Error('failed to convert database objects into json'));
+  // } catch (err) {
+  //   return next(err);
+  // }
+});
+
+
+//team members JSON
+router.get('/gallery', (req, res) => {
+  
+  //find() method after models have been built ///
+
+  // Image.find({}, (err, images) => {
+  //   if (err) {
+  //     res.status(404)
+    
+  //   }
+  //   res.json(images)
+  // })
+})
+
+//subscribers JSON
+
+
+module.exports = router
