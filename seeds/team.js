b68bf630acfdd39ad252065d42b0fc93f54083cc
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-03-25 13:48:22 -0600

    added name, bio and github links to team seed

--- seeds/team.js
+++ seeds/team.js
@@ -1,31 +1,31 @@
 const team = [
   {
-    name: '',
+    name: 'Aidan',
     profilePic: '',
     title: '',
-    bio: '',
-    github: ''
+    bio: 'Web Development student at SAIT ',
+    github: 'https://github.com/Aidano93'
   },
   {
-    name: '',
+    name: 'Dustin',
     profilePic: '',
     title: '',
-    bio: '',
-    github: ''
+    bio: 'Coding and customer service are my forte!',
+    github: 'https://github.com/cowtowndusty'
   },
   {
-    name: '',
+    name: 'Joel',
     profilePic: '',
     title: '',
-    bio: '',
-    github: ''
+    bio: 'Starting out my coding journey!',
+    github: 'https://github.com/kayej22'
   },
   {
-    name: '',
+    name: 'Makayla',
     profilePic: '',
     title: '',
-    bio: '',
-    github: ''
+    bio: 'Current student at SAIT taking the Web Developing Program. If you need a beautiful and accessible website, I\'m your gal.',
+    github: 'https://github.com/MakaylaMR'
   },
 ];
 
