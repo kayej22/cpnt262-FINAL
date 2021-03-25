4abf61113d8845db2935a1650f3a6242cef719ed
Dustin Shulz <dustin.shulz@edu.sait.ca>
2021-03-25 14:27:34 -0600

    Update import.js

--- import.js
+++ import.js
@@ -2,9 +2,12 @@ const mongoose = require('./_connection.js')
 
 // Import seed data
 const dbSeed = require(`./seeds/gallery`);
-
+const dbSeed = require(`./seeds/member`);
+const dbSeed = require(`./seeds/subscriber`);
 // Define model
-const Animal = require(`./models/Image`);
+const Animal = require(`./models/Gallerie`);
+const Animal = require(`./models/Member`);
+const Animal = require(`./models/Subscriber`);
 
 Animal.insertMany(dbSeed, function(error, animal) {
   console.log('Data import completed.')
