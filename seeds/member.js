a6326ee7a18f1e1529eabc34c47dbc43fe2bebb9
Dustin Shulz <dustin.shulz@edu.sait.ca>
2021-03-26 12:11:56 -0600

    added photo paths to member.js

--- seeds/member.js
+++ seeds/member.js
@@ -1,28 +1,28 @@
 module.exports = [
   {
     name: 'Aidan',
-    profilePic: '',
+    profilePic: 'images/aidan.jpg',
     title: '',
     bio: 'Web Development student at SAIT ',
     github: 'https://github.com/Aidano93'
   },
   {
     name: 'Dustin',
-    profilePic: '',
+    profilePic: 'images/dustin.jpg',
     title: '',
     bio: 'Coding and customer service are my forte!',
     github: 'https://github.com/cowtowndusty'
   },
   {
     name: 'Joel',
-    profilePic: '',
+    profilePic: 'images/joel.jpg',
     title: '',
     bio: 'Starting out my coding journey!',
     github: 'https://github.com/kayej22'
   },
   {
     name: 'Makayla',
-    profilePic: '',
+    profilePic: 'images/makayla.jpg',
     title: '',
     bio: 'Current student at SAIT taking the Web Developing Program. If you need a beautiful and accessible website, I\'m your gal.',
     github: 'https://github.com/MakaylaMR'
