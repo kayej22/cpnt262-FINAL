d6bff87d96f70eb7312029a5fe09c4f0098940f8
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-03-25 18:44:56 -0600

    did forEach() in team.js

--- public/js/team.js
+++ public/js/team.js
@@ -6,6 +6,44 @@ fetch('api/v0/team')
 
   return res.json()
 })
-.then((team) => {
-  //will fill out when im not tired
+.then((members) => {
+  
+  const section = document.querySelector('section');
+  //append is used to nest elements into other elements. Eg: section.appendChild(figure), the figure element will become nested into the section element. HTML terms:
+//<section>
+//  <figure></figure>
+//</section>
+members.forEach((member) => {
+  //Creating a set of figure elements for each object inside array list.
+    const figure = document.createElement('figure');
+    section.appendChild(figure);
+  
+  //Creating h2 element to nest into the figures, this is the name.
+    const name = document.createElement('h2');
+    name.innerHTML = member.name;
+    figure.appendChild(name);
+
+  //Creating h3 element to nest into the figures, this is the title.
+  const title = document.createElement('h3');
+  title.innerHTML = member.title;
+  figure.appendChild(title);
+
+  // Creating anchor element on image
+    const github = document.createElement('a');
+    github.href = member.github;
+    figure.appendChild(github);
+
+  //Creating img element for the profile pic
+    const img = document.createElement('img');
+    img.src = member.profilePic;
+    github.appendChild(img);
+
+  //Creating a figcaption element this includes the persons bio. 
+    const figCaption = document.createElement('figcaption')
+    figCaption.innerHTML = `${member.bio}`;
+    figure.appendChild(figCaption);
+  });
 })
+.catch((error) => {
+  console.log(error)
+});
